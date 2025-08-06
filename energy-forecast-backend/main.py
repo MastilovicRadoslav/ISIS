d6dbe053d6e6d67dd01be3bf94c65a88ee64fd0c
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-08-06 14:45:37 +0200

    main.py

--- energy-forecast-backend/main.py
+++ energy-forecast-backend/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+from app.routes.upload_routes import upload_bp
+
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173"])
 
-@app.route("/")
-def home():
-    return "Energy Forecast Backend is running."
+app.register_blueprint(upload_bp, url_prefix='/api')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
